b976d02a28409375bc2a7da8664a164992dffc12
Oleksandr Siukov <as-service2008@ukr.net>
2023-03-24 11:05:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 
 import fetchCountries from './js/fetchCountries';
-//import messageOver from './js/message';
 
 var debounce = require('lodash.debounce');
 
@@ -45,24 +44,6 @@ function markupCountryList(data) {
   countryList.append(...itemsCountryList(data));
 }
 
-// const itemsCountryList = data => {
-//   return data.map(item => {
-//     const newItem = document.createElement('li');
-//     const newImg = document.createElement('img');
-//     const newH = document.createElement('h4');
-//     newItem.append(newImg, newH);
-
-//     newItem.classList.add('country-item');
-//     newImg.src = item.flags.svg;
-//     newImg.alt = item.name.official;
-//     newImg.width = '50';
-//     newItem.value = item.name.official;
-//     newH.textContent = item.name.official;
-
-//     return newItem;
-//   });
-// };
-
 const itemsCountryList = data => {
   return markupLineCountry(data);
 };
@@ -79,7 +60,7 @@ function markupLineCountry(data) {
     newImg.alt = item.name.official;
     newItem.value = item.name.official;
     newH.textContent = item.name.official;
-    // console.log(newItem);
+
     return newItem;
   });
 }
@@ -87,39 +68,6 @@ function markupLineCountry(data) {
 function markupCountryInfo(data) {
   countryInfo.append(...itemCountryInfo(data));
 }
-
-// const itemCountryInfo = data => {
-//   return data.map(item => {
-//     const contentDIV = document.createElement('ul');
-//     const newItem = document.createElement('li');
-//     const newImg = document.createElement('img');
-//     const newH = document.createElement('h3');
-//     newItem.append(newImg, newH);
-//     newItem.classList.add('country-item');
-//     newImg.src = item.flags.svg;
-//     newImg.alt = item.name.official;
-//     newImg.width = '50';
-//     newItem.value = item.name.official;
-//     newH.textContent = item.name.official;
-
-//     newItem.classList.add('countryItemSize');
-//     newImg.width = '100';
-
-//     const itemCapital = document.createElement('li');
-//     const itemPopulation = document.createElement('li');
-//     const itemLanguages = document.createElement('li');
-
-//     const languages = Object.values(item.languages);
-
-//     itemCapital.innerHTML = `<span class="nameItem">Capital :</span> <span class="contentItem">${item.capital}</span> `;
-//     itemPopulation.innerHTML = `<span class="nameItem">Population :</span> <span class="contentItem">${item.population}</span> `;
-//     itemLanguages.innerHTML = `<span class="nameItem">Languages :</span> <span class="contentItem">${languages}</span> `;
-
-//     contentDIV.append(newItem, itemCapital, itemPopulation, itemLanguages);
-
-//     return contentDIV;
-//   });
-// };
 
 const itemCountryInfo = data => {
   return data.map(item => {
